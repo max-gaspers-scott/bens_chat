a78939335a32be24d1ea290aee7b1dd4ee22f5cf
maximillian gaspers scott <gaspersscottma@gmail.com>
2026-05-15 16:45:01 -0400

    new cli flow

--- chat-cli/src/main.rs
+++ chat-cli/src/main.rs
@@ -1,21 +1,23 @@
 use reqwest::{self, Client, Proxy};
 use serde::Deserialize;
+use std::collections::hash_map;
 use std::fs::OpenOptions;
 use std::io::{LineWriter, Write};
 use std::path::PathBuf;
+use std::ptr::hash;
 use std::{collections::HashMap, os::unix::process};
 use std::{fs, io};
 use uuid::Uuid;
 
 async fn get_health() -> Result<(), reqwest::Error> {
-    let url = "http://localhost:8081/health";
+    let url = "http://localhost:9821/health";
     let body = reqwest::get(url).await?.text().await?;
     println!("resp is: {body:?}");
     Ok(())
 }
 
 // async fn get_username(token: String, user_id: String) -> Resutl<String, reqwest::Error> {
-//     let url = format!("http://localhost:8081/users");
+//     let url = format!("http://localhost:9821/users");
 // }
 
 #[derive(Debug, serde::Deserialize)]
@@ -34,13 +36,20 @@ struct Message {
     username: String,
 }
 
-async fn get_messages(token: String, chat_id: Uuid) -> Result<Vec<Message>, reqwest::Error> {
+async fn get_messages(
+    login: &LoginPayload,
+    chat_id: &Uuid,
+) -> Result<Vec<Message>, reqwest::Error> {
     println!("chat-id id: {:?}", chat_id);
-    let url = format!("http://localhost:8081/messages?chat_id={}", chat_id);
+    let url = format!("http://localhost:9821/messages?chat_id={}", chat_id);
 
     let client = reqwest::Client::new();
 
-    let res = client.get(url).bearer_auth(token).send().await?;
+    let res = client
+        .get(url)
+        .bearer_auth(login.token.clone())
+        .send()
+        .await?;
 
     let message_responce: MessageResponce = res.json().await.unwrap();
     Ok(message_responce.payload)
@@ -68,7 +77,7 @@ async fn get_chats(user_info: &LoginPayload) -> Result<ChatResponce, reqwest::Er
     println!("{}", user_info.user_id);
     println!("{}", user_info.token);
     let url = format!(
-        "http://localhost:8081/user-chats?user_id={}",
+        "http://localhost:9821/user-chats?user_id={}",
         user_info.user_id
     );
 
@@ -96,9 +105,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "chats" => {
                 let chats_raw = get_chats(&login);
                 let chats = chats_raw.await.unwrap().data;
+                let mut hashmap = HashMap::new();
+
                 for c in chats {
-                    println!("name: {}", c.chat_name);
-                    println!("id: {}", c.chat_id);
+                    println!("chat: {}", c.chat_name);
+                    hashmap.insert(c.chat_name, c.chat_id);
+                }
+
+                let mut buff = String::new();
+                println!("what chat do you want to see");
+                std::io::stdin().read_line(&mut buff)?;
+                let input = buff.trim();
+                let selected_id = hashmap.get(input).unwrap();
+                let messages = get_messages(&login, selected_id).await.unwrap();
+                print!("{}[2J{}[1;1H", 27 as char, 27 as char);
+                for m in messages {
+                    println!("{}: {}", m.sender_id, m.content);
                 }
             }
             "health" => {

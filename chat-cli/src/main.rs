b96f3771e2b86bc61e81294ec2ad3abf4732fbfd
maximillian gaspers scott <gaspersscottma@gmail.com>
2026-08-12 23:06:24 -0400

    im dumb

--- chat-cli/src/main.rs
+++ chat-cli/src/main.rs
@@ -26,8 +26,8 @@ use viuer::print;
 
 // should be in env, but this will work for now
 // const PORT: u32 = 8081;
-const BASE_URL: &str = "http://localhost:8081"; //9821
-// const BASE_URL: &str = "https://bens-chat.team-stingray.com";
+// const BASE_URL: &str = "http://localhost:8081"; //9821
+const BASE_URL: &str = "https://bens-chat.team-stingray.com";
 
 use std::sync::RwLock;
 
